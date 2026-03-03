61c0075a53f1dd7fe33db1e83da5e98b581a9ef0
Subayr <subayrabdulkadir@gmail.com>
2026-03-03 15:34:55 +0100

    removed hello world

--- Gt7/Program.cs
+++ Gt7/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
         Console.WriteLine("Hello, github!");
         Console.WriteLine("Hej");
         Console.WriteLine("A");
